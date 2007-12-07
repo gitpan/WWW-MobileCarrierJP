d78ffddef176a8c90669018beb0e54f0c007e14a
MATSUNO★Tokuhiro <tokuhirom+cpan@gmail.com>
2007-12-07 00:24:04 +0000

    Import of TOKUHIROM/WWW-MobileCarrierJP-0.01 from CPAN.
    gitpan-cpan-distribution: WWW-MobileCarrierJP
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         TOKUHIROM/WWW-MobileCarrierJP-0.01.tar.gz
    gitpan-cpan-author:       TOKUHIROM
    gitpan-cpan-maturity:     released

---
+++ lib/WWW/MobileCarrierJP.pm
@@ -0,0 +1,37 @@
+package WWW::MobileCarrierJP;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+=head1 NAME
+
+WWW::MobileCarrierJP - scrape mobile carrier information
+
+=head1 WARNINGS
+
+THIS SOFTWARE IS STILL UNDER ALPHA STATUS.DON'T USE ME :)
+
+=head1 DESCRIPTION
+
+Japanese Mobile Phone Carrier doesn't feed any information by the machine readable format :(
+
+This is good wrapper for this problem.
+
+This module makes machine readable format from html :)
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno <tokuhirom gmail com>
+
+=head1 COPYRIGHT
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+=cut
+
+1;
