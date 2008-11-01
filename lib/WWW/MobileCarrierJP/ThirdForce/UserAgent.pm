03515a7de2d57d822a96d2c037a6352a34454a00
MATSUNO★Tokuhiro <tokuhirom+cpan@gmail.com>
2008-11-01 05:31:04 +0000

    Import of TOKUHIROM/WWW-MobileCarrierJP-0.21 from CPAN.
    gitpan-cpan-distribution: WWW-MobileCarrierJP
    gitpan-cpan-version:      0.21
    gitpan-cpan-path:         TOKUHIROM/WWW-MobileCarrierJP-0.21.tar.gz
    gitpan-cpan-author:       TOKUHIROM
    gitpan-cpan-maturity:     released

--- lib/WWW/MobileCarrierJP/ThirdForce/UserAgent.pm
+++ lib/WWW/MobileCarrierJP/ThirdForce/UserAgent.pm
@@ -3,7 +3,7 @@ use WWW::MobileCarrierJP::Declare;
 
 parse_one( 
     urls => ['http://creation.mb.softbank.jp/terminal/index.html'],
-    xpath => q(//tr[@bgcolor="#FFFFFF"]/td[@rowspan="4"]/..),
+    xpath => q(//tr[@bgcolor="#FFFFFF"]/td[@rowspan="5"]/..),
     scraper => scraper {
         col 1 => 'model'      => 'TEXT';
         col 2 => 'user_agent' => 'TEXT';
@@ -15,12 +15,21 @@ __END__
 
 =head1 NAME
 
-WWW::MobileCarrierJP::ThirdForce::UserAgent - get UserAgent informtation from ThirdForce site.
+WWW::MobileCarrierJP::ThirdForce::UserAgent - get UserAgent informtation from ThirdForce site.(OBSOLETE)
 
 =head1 SYNOPSIS
 
-    use WWW::MobileCarrierJP::ThirdForce::UserAgent;
-    WWW::MobileCarrierJP::ThirdForce::UserAgent->scrape();
+    (OBSOLETE)
+
+=head1 DESCRIPTION
+
+THIS MODULE IS OBSOLETE.
+
+YOU SHOULD USE ThirdForce::HTTPHeader & ThirdForce::Service.
+
+This module is no longer supported.
+
+You should also strongly avoid the use of this module.
 
 =head1 AUTHOR
 
